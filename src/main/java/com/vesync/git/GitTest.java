7c07ea877aa9287f1f595bf2f3ccaf39e61405b4
jim <jimliu@etekcity.com.cn>
2021-09-06 13:41:25 +0800

    first commit

---
+++ src/main/java/com/vesync/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.vesync.git;
+
+/**
+ * @Author jim
+ * @Date 2021/9/6
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
